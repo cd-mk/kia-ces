a5b660de1ab5373c084e7116c98f1719a5b029a7
안홍선 <37172347+cd-hsahn@users.noreply.github.com>
2018-12-12 11:27:00 +0900

    detail 컴포넌트 변경 완료 (#28)

--- emotive-driving/component/ces_2019_details/js/ces_2019_details.js
+++ emotive-driving/component/ces_2019_details/js/ces_2019_details.js
@@ -15,8 +15,55 @@ var cesScrollEffect = function() {
   });
 };
 
+var cesDetailsSlide = function() {
+  $('.details-list').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: false,
+    mobileFirst: true,
+    arrows: false,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: 'unslick'
+      }
+    ]
+  });
+
+  $(window).on('resize', function() {
+    $('.details-list').slick('resize');
+  });
+};
+
+var cesDetailSlide = function() {
+  var $target = $('.details-list');
+  var slideSetting = {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,
+        arrows: false,
+        dots: true
+      };
+  
+  $(window).on('load resize', function() {
+    if ($(window).width() > 768) {
+      if ($target.hasClass('slick-initialized')) {
+        $target.slick('unslick');
+      }
+      return;
+    }
+    if (!$target.hasClass('slick-initialized')) {
+      return $target.slick(slideSetting);
+    }
+  });
+};
+
 $(document).ready(function() {
   if ($('.ces-onScreen').length) {
     cesScrollEffect();
+  }
+  if ($('.ces_2019_details').length) {
+    cesDetailSlide();
   }
 });
